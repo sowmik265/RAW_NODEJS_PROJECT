0cbaf83682652292a35cc79abd4f326fd30aeef2
Tanvir Ahamed Sowmik <111014007+sowmik265@users.noreply.github.com>
2023-08-22 13:23:56 +0600

    project day2

--- index.js
+++ index.js
@@ -2,9 +2,41 @@
 const http = require("http");
 const { handleReqRes } = require("./helpers/handleReqRes");
 const environment = require("./helpers/environments");
+const data = require("./lib/data");
 
 //app object - module scaffolding
 const app = {};
+
+//testing file system
+// data.create(
+//   "test",
+//   "newFile",
+//   {
+//     name: "messi",
+//     country: "argentina",
+//     jersey: 10,
+//   },
+//   (err) => {
+//     console.log("error is", err);
+//   }
+// );
+
+// data.read("test", "newFile", (err, result) => {
+//   console.log(err, result);
+// });
+
+// data.update(
+//   "test",
+//   "newFile",
+//   { name: "lulus", surname: "polus" },
+//   (err) => {
+//     console.log(err);
+//   }
+// );
+
+data.delete("test", "newFile", (err) => {
+  console.log(err);
+});
 
 // create server
 app.createServer = () => {
